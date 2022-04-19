dd093cbcc3f3d8af60053c502c960db59eb2977c
Kayla Wallace <ktwallace511@gmail.com>
2022-04-19 13:09:02 +0100

    UI updates
    Added pause menu to canvas + small update for flying enemy

--- Assets/Scripts/EnemyScripts/PatrollerEnemy.cs
+++ Assets/Scripts/EnemyScripts/PatrollerEnemy.cs
@@ -9,17 +9,19 @@ public class PatrollerEnemy : MonoBehaviour
     [SerializeField] Transform startPos;
     Vector3 nextPos;
     [SerializeField] float speed;
-
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         nextPos = startPos.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Patrol();
+        Flip();
     }
 
     void Patrol()
@@ -27,12 +29,31 @@ public class PatrollerEnemy : MonoBehaviour
         if (transform.position == pos1.position)
         {
             nextPos = pos2.position;
+            // transform.eulerAngles = new Vector3(0, 180, 0);
         }
         else if (transform.position == pos2.position)
         {
             nextPos = pos1.position;
+            // transform.eulerAngles = new Vector3(0, -180, 0);
         }
 
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    void Flip() 
+    {
+        // print(rb.velocity.x);
+        if (nextPos == pos1.position)
+        {
+            // moving left 
+            // print("moving left");
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (nextPos == pos2.position)
+        {
+            // moving right 
+            // print("moving right");
+            transform.eulerAngles = new Vector3(0, -180, 0);
+        }
     }
 }
